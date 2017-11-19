7ef8a6cf8f236aea26483e861703e12662f8b09e
buleHome4y <1799560186@qq.com>
2017-11-19 19:54:46 +0800

    添加输出语句：嘿 嘿嘿！

--- src/day01/helloWorld.java
+++ src/day01/helloWorld.java
@@ -2,10 +2,11 @@ package day01;
 
 public class helloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 
 		System.out.println("Hello World!");
 		System.out.println("ÄãºÃ£¡");
+		System.out.println("ºÙ ºÙºÙ£¡");
 	}
 
 }
